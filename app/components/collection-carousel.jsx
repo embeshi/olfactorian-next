0cbc475ef47ef5d4d9b2af968dd2e96254ad0a08
embeshi <nintenshi98@gmail.com>
2024-03-22 10:59:15 -0400

    increase height of carousel

--- app/components/collection-carousel.jsx
+++ app/components/collection-carousel.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import * as React from "react"
 
 import { Card, CardContent } from "@/components/ui/card"
@@ -11,13 +13,18 @@ import {
 
 export function CollectionCarousel() {
   return (
-    <Carousel className="p-8 mx-12">
+    <Carousel 
+      className="p-8 mx-12" 
+      options={{loop: true}}
+      autoPlay={true}
+      // 
+    >
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
           <CarouselItem key={index}>
             <div className="p-1">
               <Card>
-                <CardContent className="flex items-center h-72 justify-center p-6">
+                <CardContent className="flex items-center h-80 justify-center p-6">
                   <span className="text-4xl font-semibold">{index + 1}</span>
                 </CardContent>
               </Card>
